e734c2c1e0b60947d1ce73bc95ea97a76326fb0c
P97Danny <P97Danny@users.noreply.github.com>
2022-12-06 10:22:10 +0100

    Trying some stuff

--- StudentProfile/Main.java
+++ StudentProfile/Main.java
@@ -2,4 +2,13 @@ package StudentProfile;
 
 public class Main {
     
+    public static void main(String[] args){
+
+        // Create a few students
+        StudentProfile student1 = new StudentProfile(null, null, 0, 0, null);
+        
+    }
+    // Create a few students
+    // Behavior that increases the graduate year of a student by one when a certain input / change is given
+
 }
