2313aa5282958b320560b3c23dab6013b3b29c58
P97Danny <P97Danny@users.noreply.github.com>
2022-12-06 10:29:06 +0100

    Update Main.java

--- StudentProfile/Main.java
+++ StudentProfile/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
 
         // Create a few students
-        
+        StudentProfile student1 = new StudentProfile("Danny", "Peters", 2026, 6.32, "Software Engineering");
     }
     // Create a few students
     // Behavior that increases the graduate year of a student by one when a certain input / change is given
