7d321f84f64477b5257778a7c74cc96bda2e92fc
P97Danny <P97Danny@users.noreply.github.com>
2022-12-07 09:12:56 +0100

    Update EvenOrOdd.java

--- LinkedIn Learning Challanges/Beginner/EvenOrOdd.java
+++ LinkedIn Learning Challanges/Beginner/EvenOrOdd.java
@@ -1,5 +1,29 @@
 package Beginner;
 
+import java.util.Scanner;
+
 public class EvenOrOdd {
-    
+
+    public static boolean CheckEven(int number) {
+        // TODO: implement functionality
+        
+        // boolean isEven;
+        // if(number %2 == 0){
+        //     isEven = false;
+        // }
+        // else{
+        //     isEven = true;
+        // }
+
+        return number %2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println("Is the number even? " + CheckEven(userNum));
+    }
 }
