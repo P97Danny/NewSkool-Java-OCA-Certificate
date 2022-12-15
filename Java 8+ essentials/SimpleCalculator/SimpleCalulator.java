b6b2cdb2970c28b55009e8166c9d57a2eb67fe1a
P97Danny <P97Danny@users.noreply.github.com>
2022-12-15 15:46:22 +0100

    Update SimpleCalulator.java

--- Java 8+ essentials/SimpleCalculator/SimpleCalulator.java
+++ Java 8+ essentials/SimpleCalculator/SimpleCalulator.java
@@ -3,45 +3,19 @@ package SimpleCalculator;
 import java.util.Scanner;
 
 public class SimpleCalulator {
-    
-    Scanner scanner = new Scanner(System.in);
 
-    protected void printTekst() {
 
-        System.out.println("This simple programm will ask for two different numbers, and than will add those numbers");
-    }
+    public static void main(String args[]) {
 
-    protected int getFirstNumber() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("This programm will add up two given numbers.");
         System.out.print("Enter the first number: ");
         int firstNumber = scanner.nextInt();
         System.out.print("\n");
-
-        return firstNumber;
-    }
-
-    protected int getSecondNumber() {
-
-        System.out.print("Enter second number: ");
+        System.out.print("Enter the number to be added: ");
         int secondNumber = scanner.nextInt();
         System.out.print("\n");
-
-        return secondNumber;
-    }
-
-    protected int calculateSum(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
-    }
-
-    protected void printSum() {
-
-        int numberSum = calculateSum(getFirstNumber(), getSecondNumber());
-        int firstNumber =  getFirstNumber();
-        int secondNumber = getSecondNumber();
-        System.out.println(firstNumber + " + " + secondNumber + " = " + numberSum);
-    }
-
-    public static void main(String args[]) {
-        
+        System.out.println(firstNumber + " + " + secondNumber + " = " + (secondNumber + firstNumber));
     }
 }
